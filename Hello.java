2eb59c2214a9d51b48dc968c76ea197e81c32034
SalSidoti <69947112+SalSidoti@users.noreply.github.com>
2020-08-19 22:26:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/************************************************************
+ * Java Program Name
+ ************************************************************
+ * Sal Sidoti
+ * 8/29/2020
+ * 255 C90
+ ***********************************************************/
+
+public class Hello {
+
+   public static void main(String[] args) {
+
+     System.out.println("<Sal>");
+     System.out.println("<Lab 01>");
+     System.out.println("<Introduction to the Course>");
+     System.out.println("<255 C90>");
+     System.out.println();
+     }
+ }     
